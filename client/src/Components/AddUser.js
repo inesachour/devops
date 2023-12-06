38624c6239edb939f9b6fa3f3f2b126627c492f1
inesachour <ines.achour@insat.ucar.tn>
2023-12-06 16:01:15 +0100

    dns 2

--- client/src/Components/AddUser.js
+++ client/src/Components/AddUser.js
@@ -28,7 +28,7 @@ const AddUser = ({ getUsersData }) => {
   //create new user function call
   const createUser = (values) => {
     axios
-      .post(`http://backend:5000/create-user`, values)
+      .post(`http://backend.default.svc.cluster.local:5000/create-user`, values)
       .then((res) => {
         getUsersData();
         toast({
