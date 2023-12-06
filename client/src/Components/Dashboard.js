ce0c3d9b326af5bf8d8a6ec6d1b6c8062aab7764
inesachour <ines.achour@insat.ucar.tn>
2023-12-06 09:20:15 +0100

    port 2

--- client/src/Components/Dashboard.js
+++ client/src/Components/Dashboard.js
@@ -26,7 +26,7 @@ const Dashboard = () => {
   //get users Data
   const getUsersData = () => {
     axios
-      .get("http://backend:31000/users")
+      .get("http://backend:5000/users")
       .then((res) => {
         setEmployeeData(res.data.data);
         setLoading(false);
